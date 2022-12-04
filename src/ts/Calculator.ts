f0090e5984c0bdf13a422cb6ef93534bbadad809
svetlanaizmalkina <svetlanaizmalkin@gmail.com>
2022-12-04 17:58:58 +0600

    handling multiple operations at a time

--- src/ts/Calculator.ts
+++ src/ts/Calculator.ts
@@ -1,10 +1,8 @@
 export class Calculator{
     private currentOperationTextElement:Element;
     private previousOperationTextElement:Element;
-
-    private previousOperand = '';
-    private currentOperand = '';
-    private operation = '';
+    private previousOperands:Array<{type:string, value:string}> = new Array();
+    private operands:Array<{type:string, value:string}> = new Array();
     private computed:number|null = null;
 
     constructor(previousOperationTextElement:Element, currentOperationTextElement:Element){
@@ -13,74 +11,88 @@ export class Calculator{
     }
 
     public appendNumber(number:string){
-        if(this.currentOperand == '0'){
-            this.currentOperand = number;
+        let lastOperand = this.operands[this.operands.length - 1];
+
+        if(lastOperand?.type === 'number'){
+            lastOperand.value += number
         } else {
-            this.currentOperand += number;
+            if(this.operands.length === 1 && lastOperand.value === '-'){
+                lastOperand.type = 'number';
+                lastOperand.value += number;
+            } else {
+                this.operands.push({type: 'number', value: number})
+            }
         }
     }
 
     public appendOperation(operand:string){
-        if(this.computed == null){
-            this.previousOperand = this.currentOperand;
+        let lastOperand = this.operands[this.operands.length - 1];
+        this.computed = null
+        if(lastOperand?.type === 'operation'){
+            lastOperand.value = operand
         } else {
-            this.previousOperand = this.computed?.toString();
-            this.computed = null;
+            this.operands.push({type: 'operation', value: operand})
         }
-        this.currentOperand = '';
-        this.operation = operand;
     }
 
     public appendComma(){
-        if(this.computed == null){
-            if(this.currentOperand.charAt(this.currentOperand.length-1) != '.'){
-                this.currentOperand += '.'
-            }
-        } else{
-            this.currentOperand = this.computed.toString();
-            if(this.currentOperand.charAt(this.currentOperand.length-1) != '.'){
-                this.currentOperand += '.'
-                this.computed = null;
-            }
+        let lastOperand = this.operands[this.operands.length - 1]
+        if(lastOperand?.type === 'number' && lastOperand?.value.charAt(lastOperand?.value?.length-1) != '.'){
+            lastOperand.value += '.'
         }
     }
 
     public compute(){
-        let previousOperand = parseFloat(this.previousOperand)
-        let currentOperand = parseFloat(this.currentOperand)
-        
-        switch(this.operation){
-            case '+': this.computed = previousOperand + currentOperand; break;
-            case '-': this.computed = previousOperand - currentOperand; break;
-            case '*': this.computed = previousOperand * currentOperand; break;
-            case '÷': if(currentOperand != 0) { this.computed = previousOperand / currentOperand; }; 
-        }
+        let previousOperand:number|null = null;
+        let currentOperand:number|null = null;
+        let operation:string|null = null;
+        let computed:number|null = null;
 
-        this.previousOperand = '';
-        this.operation = '';
+        this.operands.forEach(({type, value}) => {
+            if(type === 'number'){
+                if(previousOperand === null) previousOperand = parseFloat(value);
+                else if (currentOperand === null) currentOperand = parseFloat(value);
+            } else {
+                if(operation === null) operation = value;
+            }
+
+            if(computed === null
+                && previousOperand !== null
+                && currentOperand !== null
+                && operation !== null){
+                    switch(operation){
+                        case '+': computed = parseFloat((previousOperand + currentOperand).toFixed(10)); break;
+                        case '-': computed = parseFloat((previousOperand - currentOperand).toFixed(10)); break;
+                        case '*': computed = parseFloat((previousOperand * currentOperand).toFixed(10)); break;
+                        case '÷': if(currentOperand != 0) { computed = parseFloat((previousOperand / currentOperand).toFixed(10)); }; 
+                    }
+
+                previousOperand = computed;
+                currentOperand = null;
+                operation = null;
+                computed = null;
+            }
+        })
+
+        this.computed = previousOperand;
+        if(previousOperand !== null){
+            this.previousOperands = this.operands;
+            this.operands = new Array({type: 'number', value: previousOperand.toString()})
+        }
     }
 
     public clear(){
-        this.previousOperand = '';
-        this.currentOperand = '';
-        this.operation = '';
+        this.operands = new Array();
         this.computed = null;
     }
 
     public updateScreen(){
+        this.previousOperationTextElement.innerHTML = this.previousOperands.map(operand => operand.value).join('');
+        this.currentOperationTextElement.innerHTML = this.operands.map(operand => operand.value).join('')
         if(this.computed != null){
-            this.previousOperationTextElement.innerHTML = this.currentOperationTextElement.innerHTML;
-            this.currentOperationTextElement.innerHTML = "=" + this.computed;
-        } else {
-            if(this.currentOperand == '' && this.previousOperand != ''){
-                this.previousOperationTextElement.innerHTML += '=' + this.previousOperand
-            }
-
-            if(this.previousOperand == ''){
-                this.previousOperationTextElement.innerHTML = this.previousOperand
-            }
-
-            this.currentOperationTextElement.innerHTML = this.previousOperand + this.operation + this.currentOperand;
+            this.previousOperationTextElement.innerHTML = this.previousOperands.map(operand => operand.value).join('');
+            this.currentOperationTextElement.innerHTML = '=' + this.computed;
+            this.previousOperands.push({type: 'operation', value: '='}, {type: 'number', value: this.computed.toString()})
         }
     }
 }
