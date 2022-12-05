abeaf775b46e07bc8199f8e6038ac86c4c2221c8
svetlanaizmalkina <svetlanaizmalkin@gmail.com>
2022-12-06 01:36:51 +0600

    gh-pages workflow

--- src/ts/Calculator.ts
+++ src/ts/Calculator.ts
@@ -81,38 +81,61 @@ export class Calculator {
       } else {
         if (operation === null) operation = value
       }
-
-      if (computed === null &&
-                previousOperand !== null &&
-                currentOperand !== null &&
-                operation !== null) {
-        switch (operation) {
-          case operations.plus: computed = parseFloat((previousOperand + currentOperand).toFixed(10)); break
-          case operations.minus: computed = parseFloat((previousOperand - currentOperand).toFixed(10)); break
-          case operations.multiply: computed = parseFloat((previousOperand * currentOperand).toFixed(10)); break
-          case operations.divide: if (currentOperand !== 0) { computed = parseFloat((previousOperand / currentOperand).toFixed(10)) };
-        }
-
-        previousOperand = computed
-        currentOperand = null
-        operation = null
-        computed = null
-      }
     })
 
-    this.computed = previousOperand
+    if (computed === null && previousOperand !== null && currentOperand !== null && operation !== null) {
+      computed = parseFloat(this.computeSequenceByPriority(this.computeSequenceArray).value)
 
-    if (previousOperand !== null) {
+      currentOperand = null
+      operation = null
+    }
+
+    this.computed = computed
+
+    if (computed !== null) {
       this.previousComputeSequenceArray = this.computeSequenceArray
-      this.computeSequenceArray = new Array({ type: 'number', value: (previousOperand as number).toString() })
       if (this.computed != null) {
-        this.previousComputeSequenceArray.push({ type: 'operation', value: '=' }, { type: 'number', value: (this.computed as number).toString() })
+        this.previousComputeSequenceArray.push({ type: 'operation', value: '=' }, { type: 'number', value: this.computed.toString() })
       }
+
+      this.computeSequenceArray = new Array({ type: 'number', value: computed.toString() })
     }
   }
 
   public clear (): void {
     this.computeSequenceArray = []
     this.computed = null
+  }
+
+  private computeSequenceByPriority (computeSequenceArray: computeSequenceType): { type: string, value: string } {
+    const operationsArray = [operations.multiply, operations.divide, operations.minus, operations.plus]
+
+    operationsArray.forEach(operationValue => {
+      while (computeSequenceArray.find(computeItem => computeItem.value === operationValue) != null) {
+        computeSequenceArray.forEach((item, index) => {
+          if (item.value === operationValue) {
+            const operation = item
+            const previousOperand = computeSequenceArray[index - 1]
+            const currentOperand = computeSequenceArray[index + 1]
+            const computed = this.copmuteTwoOperands(operation.value, parseFloat(previousOperand.value), parseFloat(currentOperand.value))
+            if (computed != null) {
+              computeSequenceArray.splice(index - 1, 3, { type: 'number', value: computed.toString() })
+            }
+          }
+        })
+      }
+    })
+
+    return computeSequenceArray[0]
+  }
+
+  private copmuteTwoOperands (operation: string, previousOperand: number, currentOperand: number): number | null {
+    switch (operation) {
+      case operations.plus: return parseFloat((previousOperand + currentOperand).toFixed(10))
+      case operations.minus: return parseFloat((previousOperand - currentOperand).toFixed(10))
+      case operations.multiply: return parseFloat((previousOperand * currentOperand).toFixed(10))
+      case operations.divide: if (currentOperand !== 0) { return parseFloat((previousOperand / currentOperand).toFixed(10)) };
+    }
+    return null
   }
 }
