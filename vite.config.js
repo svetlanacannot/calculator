c6b36d5dc51d99d9b964a828ceaacb6b473eb577
svetlanaizmalkina <svetlanaizmalkin@gmail.com>
2022-12-06 02:08:21 +0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 /** @type {import('vite').UserConfig} */
 export default {
   root: process.cwd(),
-  base: '/calculator/',
+  base: '/',
   mode: 'development',
   plugins: [],
   publicDir: './public',
-  appType: 'spa'
+  appType: 'spa',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          return 'assets[name]-[hash][extname]'
+        },
+        entryFileNames: 'calculator/assets/[name]-[hash].js'
+      }
+    }
+  }
 }
