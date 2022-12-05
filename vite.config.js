ab6d2f67fd7a97b0ad63e8755b7f1a48d3a64049
svetlanaizmalkina <svetlanaizmalkin@gmail.com>
2022-12-05 21:19:18 +0600

    vite config

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+/** @type {import('vite').UserConfig} */
+export default {
+  root: process.cwd(),
+  base: '/',
+  mode: 'development',
+  plugins: [],
+  publicDir: './public',
+  appType: 'spa'
+}
